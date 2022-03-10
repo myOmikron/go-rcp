be8a45bc2c7dee312a4f42b7f9da48c7c74dd84f
myOmikron <git@omikron.dev>
2022-03-10 15:28:53 +0100

    Added GetChecksum

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/myOmikron/gorcp
+
+go 1.17
---
+++ rcp.go
@@ -0,0 +1,51 @@
+package gorcp
+
+import (
+	"crypto/sha512"
+	"fmt"
+	"sort"
+	"strconv"
+	"time"
+)
+
+type RCPConfig struct {
+	UseTimeComponent bool
+	SharedSecret     string
+}
+
+func GetChecksum(request map[string]string, salt string, config *RCPConfig) (checksum string) {
+	var currentTimestamp int64
+	if config.UseTimeComponent {
+		currentTimestamp = time.Now().UTC().Unix()
+	}
+
+	// Create sorted slice of keys
+	keys := make([]string, 0, len(request))
+	for _, k := range request {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+
+	var builder string
+	for _, k := range keys {
+		// Append key + value
+		builder += k + request[k]
+	}
+
+	// Append shared secret
+	builder += config.SharedSecret
+
+	if config.UseTimeComponent {
+		// Append timestamp
+		builder += strconv.FormatInt(currentTimestamp, 64)
+	}
+
+	// Prefix with salt
+	builder = salt + builder
+
+	hash := sha512.New()
+	hashed := hash.Sum([]byte(builder))
+
+	checksum = fmt.Sprintf("%x", hashed)
+	return
+}

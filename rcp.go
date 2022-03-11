228ab3f4c5ac5377b7368057adad363cf26e2d84
myOmikron <git@omikron.dev>
2022-03-11 01:29:25 +0100

    Added ValidateChecksum as well as tests for everything

--- rcp.go
+++ rcp.go
@@ -8,27 +8,81 @@ import (
 )
 
 type RCPConfig struct {
-	UseTimeComponent bool
 	SharedSecret     string
+	UseTimeComponent bool
+	TimeDelta        int64
 }
 
-func GetChecksum(request *map[string]string, salt string, config *RCPConfig) (checksum string) {
+func sortKeys(m *map[string]string) (keys []string) {
+	keys = make([]string, 0, len(*m))
+	for k, _ := range *m {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+	return
+}
+
+func ValidateChecksum(m *map[string]string, checksum string, salt string, config *RCPConfig) (b bool) {
 	var currentTimestamp int64
 	if config.UseTimeComponent {
 		currentTimestamp = time.Now().UTC().Unix()
 	}
 
 	// Create sorted slice of keys
-	keys := make([]string, 0, len(*request))
-	for _, k := range *request {
-		keys = append(keys, k)
-	}
-	sort.Strings(keys)
+	keys := sortKeys(m)
 
 	var builder string
 	for _, k := range keys {
 		// Append key + value
-		builder += k + (*request)[k]
+		builder += k + (*m)[k]
+	}
+
+	// Append shared secret
+	builder += config.SharedSecret
+
+	hash := sha512.New()
+	if config.UseTimeComponent {
+		for i := -config.TimeDelta; i < config.TimeDelta; i++ {
+			// Append timestamp
+			timestamp := currentTimestamp + i
+			tmpBuilder := builder + fmt.Sprint(timestamp)
+
+			// Prefix with salt
+			tmpBuilder = salt + tmpBuilder
+
+			hash.Reset()
+			hash.Write([]byte(tmpBuilder))
+			hashed := hash.Sum(nil)
+			if fmt.Sprintf("%x", hashed) == checksum {
+				b = true
+				break
+			}
+		}
+	} else {
+		// Prefix with salt
+		builder = salt + builder
+
+		hash.Write([]byte(builder))
+		hashed := hash.Sum(nil)
+		b = fmt.Sprintf("%x", hashed) == checksum
+	}
+
+	return
+}
+
+func GetChecksum(m *map[string]string, salt string, config *RCPConfig) (checksum string) {
+	var currentTimestamp int64
+	if config.UseTimeComponent {
+		currentTimestamp = time.Now().UTC().Unix()
+	}
+
+	// Create sorted slice of keys
+	keys := sortKeys(m)
+
+	var builder string
+	for _, k := range keys {
+		// Append key + value
+		builder += k + (*m)[k]
 	}
 
 	// Append shared secret
@@ -36,14 +90,16 @@ func GetChecksum(request *map[string]string, salt string, config *RCPConfig) (ch
 
 	if config.UseTimeComponent {
 		// Append timestamp
-		builder += string(currentTimestamp)
+		builder += fmt.Sprint(currentTimestamp)
 	}
 
 	// Prefix with salt
 	builder = salt + builder
 
+	// Calculate SHA512
 	hash := sha512.New()
-	hashed := hash.Sum([]byte(builder))
+	hash.Write([]byte(builder))
+	hashed := hash.Sum(nil)
 
 	checksum = fmt.Sprintf("%x", hashed)
 	return
---
+++ rcp_test.go
@@ -0,0 +1,90 @@
+package gorcp
+
+import (
+	"testing"
+)
+
+func TestSortKeys(t *testing.T) {
+	m := make(map[string]string)
+	if len(sortKeys(&m)) != 0 {
+		t.Error("Keys should be empty")
+		return
+	}
+	m["b"] = "3"
+	m["a"] = "2"
+	sorted := sortKeys(&m)
+	if sorted[0] != "a" || sorted[1] != "b" {
+		t.Error("Sort failed")
+		return
+	}
+}
+
+func TestValidateChecksum(t *testing.T) {
+	config := &RCPConfig{
+		UseTimeComponent: false,
+		SharedSecret:     "Hallo-123",
+		TimeDelta:        5,
+	}
+	m := make(map[string]string)
+
+	// Output of reference implementation:
+	// rc_protocol.get_checksum({}, "Hallo-123", use_time_component=False)
+	checksum := "477cec82c1c05f7acd42e4c9bd354f3021a59f9a0e8f6cca451c74511a75a8ee0aa4cddcf0a966e91de09b5708d26ce2a7737b65f286a368c87e751135cdc706"
+	if !ValidateChecksum(&m, checksum, "", config) {
+		t.Error("Not time based, empty map, no salt failed")
+		return
+	}
+
+	// Output of reference implementation:
+	// rc_protocol.get_checksum({"b": "test", "a": " long test"}, "Hallo-123", salt="TestSalt", use_time_component=False)
+	checksum = "a85a29e01f295cba43de859a097b6f816826a0ef47bad9d210ab1410cc6ea8490f72a99e62c27b3aefd3b334b1a034d1b8ba1b8b0c6599c27674aeb96cebd591"
+	m["b"] = "test"
+	m["a"] = " long test"
+	if !ValidateChecksum(&m, checksum, "TestSalt", config) {
+		t.Error("Not time based, populated map, with salt failed")
+		return
+	}
+
+	config.UseTimeComponent = true
+	checksum = GetChecksum(&m, "TestSalt", config)
+	if !ValidateChecksum(&m, checksum, "TestSalt", config) {
+		t.Error("Time based, populated map, with salt failed")
+		return
+	}
+
+}
+
+func TestGetChecksum(t *testing.T) {
+	config := &RCPConfig{
+		UseTimeComponent: false,
+		SharedSecret:     "Hallo-123",
+		TimeDelta:        5,
+	}
+	m := make(map[string]string)
+
+	// Output of reference implementation:
+	// rc_protocol.get_checksum({}, "Hallo-123", use_time_component=False)
+	correct := "477cec82c1c05f7acd42e4c9bd354f3021a59f9a0e8f6cca451c74511a75a8ee0aa4cddcf0a966e91de09b5708d26ce2a7737b65f286a368c87e751135cdc706"
+	if GetChecksum(&m, "", config) != correct {
+		t.Error("rc_protocol.get_checksum({}, \"Hallo-123\", use_time_component=False) is incorrect")
+		return
+	}
+
+	// Output of reference implementation:
+	// rc_protocol.get_checksum({}, "Hallo-123", salt="TestSalt", use_time_component=False)
+	correct = "50acbd16790dc2ebcc246ea9050acf4bee79088d1a9b0a0cd9f812a3b054b7c39e6ce44aa9c6e53b6d31c9d7da527cdd9a85ecaf2f5d007533d4cde289432683"
+	if GetChecksum(&m, "TestSalt", config) != correct {
+		t.Error("rc_protocol.get_checksum({}, \"Hallo-123\", salt=\"TestSalt\", use_time_component=False) is incorrect")
+		return
+	}
+
+	// Output of reference implementation:
+	// rc_protocol.get_checksum({"b": "test", "a": " long test"}, "Hallo-123", salt="TestSalt", use_time_component=False)
+	correct = "a85a29e01f295cba43de859a097b6f816826a0ef47bad9d210ab1410cc6ea8490f72a99e62c27b3aefd3b334b1a034d1b8ba1b8b0c6599c27674aeb96cebd591"
+	m["b"] = "test"
+	m["a"] = " long test"
+	if GetChecksum(&m, "TestSalt", config) != correct {
+		t.Error("rc_protocol.get_checksum({\"b\": \"test\", \"a\": \" long test\"}, \"Hallo-123\", salt=\"TestSalt\", use_time_component=False) is incorrect")
+		return
+	}
+}
